c27a71f34f538e17c8ad4ac71d34e47f3c784735
GlaudistonNeto <glaudistonson@gmail.com>
2021-12-06 14:29:51 -0300

    going to stylize it

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,12 @@
 <template>
-<div class="container">
-  <div class="d-flex flex-column align-items-stretch flex-shrink-0 bg-white">
+  <div class="container">
+    <div class="d-flex flex-column align-items-stretch flex-shrink-0 bg-white">
       <div class="d-flex align-items-center flex-shrink-0 p-3 link-dark text-decoration-none border-bottom">
-        <input class="fs-5 fw-semibold" v-model="username">
+        <input class="fs-5 fw-semibold" v-model="username"/>
       </div>
       <div class="list-group list-group-flush border-bottom scrollarea">
         <div class="list-group-item list-group-item-action py-3 lh-tight"
-          v-for="message in messages" :key="message"
+             v-for="message in messages" :key="message"
         >
           <div class="d-flex w-100 align-items-center justify-content-between">
             <strong class="mb-1">{{ message.username }}</strong>
@@ -16,13 +16,15 @@
       </div>
     </div>
     <form @submit.prevent="submit">
-      <input class="form-control" placeholder="Write a message" v-model="message">
+      <input class="form-control" placeholder="Write a message" v-model="message"/>
     </form>
   </div>
 </template>
 
 <script>
-import {ref} from 'vue';
+import {ref, onMounted} from 'vue';
+import Pusher from 'pusher-js';
+
 export default {
   name: 'App',
   setup() {
@@ -30,8 +32,30 @@ export default {
     const messages = ref([]);
     const message = ref('');
 
-    const submit = () => {
+    onMounted(() => {
+      Pusher.logToConsole = true;
 
+
+      const pusher = new Pusher('068133b23cfaf634458b', {
+        cluster: 'us3'
+      });
+
+      const channel = pusher.subscribe('chat');
+      channel.bind('message', data => {
+        messages.value.push(data);
+      });
+    });
+
+    const submit = async () => {
+      await fetch('http://localhost:8000/api/messages', {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({
+          username: username.value,
+          message: message.value
+        })
+      })
+      message.value = '';
     }
 
     return {
