1570b283e92db0e305f19987ea9611531ea2e2d8
GlaudistonNeto <glaudistonson@gmail.com>
2021-12-08 22:26:35 -0300

    stylized chat

--- src/App.vue
+++ src/App.vue
@@ -1,74 +1,41 @@
 <template>
-  <div class="container">
-    <div class="main-bg">
-        <div class="left">
-          <img src="../assets/GeeksBay-4.jpg" alt="logo" />
-        </div>
-        <div class="right">
-          <nav>
-            <li><a href="#">Login</a></li>
-            <li><a href="#">Sign Up</a></li>
-          </nav>
-        </div>
-      <div class="geek">
-        <h1>GeekCentric</h1>
+<div class="container">
+  <div class="">
+      <div class="container d-flex align-items-center flex-shrink-0 p-3 link-dark text-decoration-none border-bottom">
+        <input class="fs-5 fw-semibold" placeholder="Name" v-model="username">
       </div>
-      <div class="post-detail">        
-        <form class="submit" @submit.prevent="submit">
-          <input class="post" placeholder="Type your favourite topic..." v-model="message"/>
-        </form>
-      </div>
-      <div class="postages list-group list-group-flush border-bottom scrollarea">
+      <div class="container list-group list-group-flush border-bottom scrollarea">
         <div class="list-group-item list-group-item-action py-3 lh-tight"
-             v-for="message in messages" :key="message"
+          v-for="message in messages" :key="message"
         >
-          <div class="post-details col-10 mb-1 small">{{ message.message }}</div>
+        <div class="d-flex w-100 align-items-center justify-content-between">
+          <strong class="mb-1">{{ message.username }}</strong>
         </div>
+        <div class="col-10 mb-1 small">{{ message.message }}</div>
       </div>
     </div>
   </div>
+  <form class="form-container" @submit.prevent="submit">
+    <input class="form-control" placeholder="Write a message" v-model="message">
+  </form>
+</div>
 </template>
 
 <script>
-import {ref, onMounted} from 'vue';
-import Pusher from 'pusher-js';
-
+import {ref} from 'vue';
 export default {
   name: 'App',
   setup() {
-    const post = ref('');
+    const username = ref('');
     const messages = ref([]);
     const message = ref('');
 
-    onMounted(() => {
-      Pusher.logToConsole = true;
+    const submit = () => {
 
-
-      const pusher = new Pusher('068133b23cfaf634458b', {
-        cluster: 'us3'
-      });
-
-      const channel = pusher.subscribe('chat');
-      channel.bind('message', data => {
-        messages.value.push(data);
-      });
-    });
-
-    const submit = async () => {
-      await fetch('http://localhost:8000/api/messages', {
-        method: 'POST',
-        headers: {'Content-Type': 'application/json'},
-        body: JSON.stringify({
-          post: post.value,
-          message: message.value
-        })
-      })
-      message.value = '';
-      post.value = '';
     }
 
     return {
-      post,
+      username,
       messages,
       message,
       submit
@@ -79,59 +46,12 @@ export default {
 
 <style scoped>
 .container {
-  background-color: #330624;
-  padding: 30px;
-  text-align: center;
-  box-sizing: border-box;
-}
-.right {
-  float: right;
-}
-.left {
-  padding-bottom: 25px;
-  float: left
-}
-h1 {
-  color: #fdb924;
-  font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
-}
-.submit {
-  margin-top: 10;
-  width: 100%;
-}
-.logo {
-  margin-left: -900px;
-}
-.post-detail {
-  margin: 1px;
-  flex-direction: row;
-}
-.post-details {
-  background-color: #330624;
-  padding: 10px;
-  box-sizing: border-box;
-  color: #fff;
-  font-size: 18px;
-  border-bottom: 3px solid #fff;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-direction: column;
-}
-.postages {
-  padding: 10px;
-  box-sizing: border-box;  
-  color: rgb(88, 88, 88);
-  margin: 0 10px;
-  border-bottom: 3px solid #ccc;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-direction: column;
-  flex-direction: row;
+  background-color:#330623;
 }
 .scrollarea {
-  min-height: 808px;
-  margin: auto;
+  min-height: 837px;
+}
+.form-container {
+  padding-bottom: 10px;
 }
 </style>
