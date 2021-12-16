acd5e5e76da2cabe1f75cf59eceadc3c255ecd12
GlaudistonNeto <glaudistonson@gmail.com>
2021-12-16 19:33:29 -0300

    ending the chat

--- src/App.vue
+++ src/App.vue
@@ -46,14 +46,14 @@ export default {
       });
     });
     const submit = async () => {
-      // await fetch('http://localhost:3333/', {
-      //   method: 'POST',
-      //   headers: {'Content-Type': 'application/json'},
-      //   body: JSON.stringify({
-      //     username: username.value,
-      //     message: message.value
-      //   })
-      // })
+      await fetch('http://localhost:3333/api/messages', {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({
+          username: username.value,
+          message: message.value
+        })
+      })
       message.value = '';
     }
 
